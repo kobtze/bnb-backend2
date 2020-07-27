d7846a57ead022df9075ba5ee893e7b6276e47e2
Kobi Donner <kobibo@gmail.com>
2020-07-28 02:01:41 +0300

    update / delete apis blocked Kobi 2:01

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -11,7 +11,7 @@ router.get('/:id', getUser)
 router.post('/', addUser)
 // router.put('/:id', updateUser)   // BUGGY
 // router.put('/:id',  requireAuth, updateUser)
-router.delete('/:id', deleteUser)
+// router.delete('/:id', deleteUser)
 // router.delete('/:id',  requireAuth, requireAdmin, deleteUser)
 
 module.exports = router
