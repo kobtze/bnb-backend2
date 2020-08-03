54d5851c8c0159e24eacb0999f44f250fa789666
Kobi Donner <kobibo@gmail.com>
2020-08-03 17:53:32 +0300

    kobi 1753

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // "dbURL": "mongodb+srv://dolev:1234@cluster0.acluv.mongodb.net/bnb_db?retryWrites=true&w=majority",
     "dbURL": "mongodb+srv://dolev:1234@cluster0.acluv.mongodb.net/bnb_db?retryWrites=true&w=majority"
 }
